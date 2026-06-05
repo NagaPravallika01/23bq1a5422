800beb42605429e1499c7ea8843997de09163b4c
NagaPravallika01 <23bq1a5422@vvit.net>
2026-06-05 10:57:35 +0530

    Testing Git

--- notification_app_be/server.js
+++ notification_app_be/server.js
@@ -1,1 +1,12 @@
-hello
+const express = require("express");
+const logger = require("../logging_middleware");
+
+const app = express();
+
+app.use(logger);
+
+app.get("/", (req, res) => {
+    res.send("Hello");
+});
+
+app.listen(3000);

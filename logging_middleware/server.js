54e8f95b34731e85f9bb4128651aeaf863396852
NagaPravallika01 <23bq1a5422@vvit.net>
2026-06-05 09:44:59 +0530

    Testing Git

---
+++ logging_middleware/server.js
@@ -0,0 +1,16 @@
+{
+    "vehicles":[
+        {
+            "TaskID":"264e638f-1c7a-ad67-9f9c-53f3d1766d37","Duration":1,"Impact":5;
+
+        },
+         {
+            "TaskID":"4b6e22ee-b4ed-a5a4-a6af-5294b0d69f37","Duration":1,"Impact":3;
+
+        },
+         {
+            "TaskID":"73ce9dca-1536-4a7a-9f1e-c67083afad61","Duration":6,"Impact":2;
+
+        },
+    ]
+}
